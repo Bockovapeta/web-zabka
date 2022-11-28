df29966d0d0514c76b67118f514fb0606394db5d
Petra Bočková <Wickky@seznam.cz>
2022-11-28 18:52:38 +0100

    oprava favikonky

--- js/main.js
+++ js/main.js
@@ -131,7 +131,7 @@ $(document).ready(function () {
         slidesToShow: 1,
         slidesToScroll: 1,
         autoplay: true,
-        autoplaySpeed: 5000,
+        autoplaySpeed: 4000,
         prevArrow: $('.arrows__left'),
         nextArrow: $('.arrows__right'),
 
