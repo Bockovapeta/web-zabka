ba39b3855b1497ae96b31d06a65e9ae527f914a2
Petra Bočková <Wickky@seznam.cz>
2022-12-08 21:23:52 +0100

    úprava main.js

--- js/main.js
+++ js/main.js
@@ -16,6 +16,30 @@ close.addEventListener('click', function () {
 
 */
 
+
+// Scrolling 
+$(document).on('click', 'a[href^="#"]', function (e) {
+    // target element id
+    let id = $(this).attr('href');
+
+    // target element
+    let $id = $(id);
+    if ($id.length === 0) {
+        return;
+    }
+
+    // prevent standard hash navigation (avoid blinking in IE)
+    e.preventDefault();
+
+    // top position relative to the document
+    let pos = $id.offset().top;
+
+    // animated top scrolling
+    $('body, html').animate({ scrollTop: pos });
+});
+
+
+
 $(document).ready(function () {
 
 
@@ -69,49 +93,29 @@ $(document).ready(function () {
 
     // validace formulářů
 
-
-    $('#brief-form').validate({
-        rules: {
-            name: "required",
-            email: {
-                required: true,
-                email: true
-            },
-
-            zprava: "required"
-        },
-        messages: {
-            name: "Uveďte vaše jméno",
-            email: "Uveďte Váš email",
-            zprava: "Napište mi zprávu"
-        }
-    });
-
-
+    //
+    //   $('#brief-form').validate({
+    //       rules: {
+    //           name: required,
+    //         email: {
+    //           required: true,
+    //         email: true
+    //            },
+    //
+    //          zprava: "required"
+    //     },
+    //    messages: {
+    //       name: "Uveďte vaše jméno",
+    //      email: "Uveďte Váš email",
+    //     zprava: "Napište zprávu"
+    //        }
+    //   });
 
 
 
 
-    // Scrolling 
-    $(document).on('click', 'a[href^="#"]', function (e) {
-        // target element id
-        let id = $(this).attr('href');
 
-        // target element
-        let $id = $(id);
-        if ($id.length === 0) {
-            return;
-        }
 
-        // prevent standard hash navigation (avoid blinking in IE)
-        e.preventDefault();
-
-        // top position relative to the document
-        let pos = $id.offset().top;
-
-        // animated top scrolling
-        $('body, html').animate({ scrollTop: pos });
-    });
 
 
 
